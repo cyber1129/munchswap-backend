9b3882ed8c93580e9a319cdc84f6666cca9a3378
cyber1129 <luckycyber1129@gmail.com>
2024-02-21 09:42:23 +0000

    Add get user info api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, forwardRef, Inject, Get } from '@nestjs/common';
+import {
+  Controller,
+  forwardRef,
+  Inject,
+  Get,
+  UseGuards,
+  HttpStatus,
+  Request,
+} from '@nestjs/common';
 
 import { AuthService } from '@src/auth/auth.service';
 import { UserService } from './user.service';
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { JwtAuthGuard } from '@src/auth/guards/jwt-auth.guard';
+import { ApiResponseHelper } from '@src/common/helpers/api-response.helper';
+import { User } from './user.entity';
 
-@Controller()
+@Controller('user')
 export class UserController {
   constructor(
     private readonly userService: UserService,
     @Inject(forwardRef(() => AuthService))
     private readonly authService: AuthService,
   ) {}
+
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ description: `Get user info`, tags: ['User'] })
+  @ApiResponse(ApiResponseHelper.success(User, HttpStatus.OK))
+  @ApiResponse(ApiResponseHelper.validationError(`Validation failed`))
+  @Get('/user-info')
+  async getUserPushedOffers(@Request() req) {
+    return this.userService.findByAddress(req.user.address);
+  }
 }
